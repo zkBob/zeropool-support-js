9dc96f387c98d36da1f06be58ec7ec3d786eceef
Dmitry Vdovin <voidxnull@gmail.com>
2021-02-02 11:36:47 +0200

    Cleanup

--- examples/near.ts
+++ examples/near.ts
@@ -1,6 +1,6 @@
 import { HDWallet, CoinType } from 'zeropool-api-js';
 import { generateMnemonic } from 'zeropool-api-js/lib/utils';
-import devConfig from 'zeropool-api-js/config.dev'; // you might prefer to use your own
+import devConfig from 'zeropool-api-js/src/config.dev'; // you might prefer to use your own
 
 async function example() {
   const mnemonic = generateMnemonic();
@@ -15,3 +15,6 @@ async function example() {
   balance = await near.getBalance();
   console.log('Balance: ', balance);
 }
+
+example()
+  .catch(e => console.error(e));
