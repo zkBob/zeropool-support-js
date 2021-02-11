c350e1e206117e6c79c90b8b20e461d1fa8883d3
Dmitry Vdovin <voidxnull@gmail.com>
2021-02-11 21:41:48 +0200

    Fix config re-exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export * as near from './coins/near';
 export { CoinType } from './coins/coin-type';
 export { HDWallet } from './hd-wallet';
 
-export * as devConfig from './config.dev';
-export * as prodConfig from './config.prod';
+export { default as devConfig } from './config.dev';
+export { default as prodConfig } from './config.prod';
 
