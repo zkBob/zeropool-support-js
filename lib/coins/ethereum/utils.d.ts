68a6fc214af079bbfc87391ed8d98c9da20d0859
Dmitry Vdovin <voidxnull@gmail.com>
2021-10-21 11:43:35 +0200

    Change ts target to es2016, update lib

--- lib/coins/ethereum/utils.d.ts
+++ lib/coins/ethereum/utils.d.ts
@@ -3,3 +3,4 @@ import { Transaction as NativeTx } from 'web3-core';
 export declare function convertTransaction(tx: NativeTx, timestamp: number, customStatus?: TxStatus): Transaction;
 export declare function toCompactSignature(signature: string): string;
 export declare function toCanonicalSignature(signature: string): string;
+export declare function toTwosComplementHex(num: bigint, numBytes: number): string;
