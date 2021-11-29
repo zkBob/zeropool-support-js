53128b2037cb3e53817003a38904b6337897ba46
Dmitry Vdovin <voidxnull@gmail.com>
2021-11-29 14:47:53 +0200

    Add logging to worker.js

--- src/worker.ts
+++ src/worker.ts
@@ -6,16 +6,20 @@ let treeParams: Params;
 
 const obj = {
   async initWasm(url: string, paramUrls: { txParams: string; treeParams: string }) {
+    console.info('Initializing web worker...');
     await init(url);
 
     const txParamsData = await (await fetch(paramUrls.txParams)).arrayBuffer();
     txParams = Params.fromBinary(new Uint8Array(txParamsData));
     const treeParamsData = await (await fetch(paramUrls.treeParams)).arrayBuffer();
     treeParams = Params.fromBinary(new Uint8Array(treeParamsData));
+
+    console.info('Web worker init complete.');
   },
 
   async proveTx(pub, sec) {
     return new Promise(async resolve => {
+      console.debug('Web worker: proveTx');
       const result = Proof.tx(txParams, pub, sec);
       resolve(result);
     });
@@ -23,6 +27,7 @@ const obj = {
 
   async proveTree(pub, sec) {
     return new Promise(async resolve => {
+      console.debug('Web worker: proveTree');
       const result = Proof.tree(treeParams, pub, sec);
       resolve(result);
     });
