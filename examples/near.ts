8dd687704ee95b72159a0f79b82a4a1535597f9b
Dmitry Vdovin <voidxnull@gmail.com>
2022-01-15 21:48:36 +0400

    Fix example

--- examples/near.ts
+++ examples/near.ts
@@ -1,12 +1,12 @@
-import { HDWallet, CoinType, devConfig } from 'zeropool-api-js';
+import { HDWallet, NetworkType, devConfig } from 'zeropool-api-js';
 import { generateMnemonic } from 'zeropool-api-js/lib/utils';
 
 async function example() {
   const mnemonic = generateMnemonic();
-  const hdWallet = new HDWallet(mnemonic, [CoinType.near], devConfig);
+  const hdWallet = new HDWallet(mnemonic, [NetworkType.near], devConfig);
 
-  const near = hdWallet.getCoin(CoinType.near, 0);
-  let balance = await near.getBalance();
+  const near = hdWallet.getNetwork(NetworkType.near, 0);
+  let balance = await near.getNetwork();
 
   console.log('Balance: ', balance);
   await near.transfer('some address', near.toBaseUnit('1'));
