cd6d072456bdedc99ba82a0a00b5542e73899677
Dmitry Vdovin <voidxnull@gmail.com>
2022-01-31 21:41:56 +0200

    Fix indentation in src/state.ts

--- src/state.ts
+++ src/state.ts
@@ -4,38 +4,38 @@ import { UserAccount, UserState } from '@/libzeropool-rs';
 import { bufToHex } from '@/utils';
 
 export class ZeroPoolState {
-    public denominator: bigint;
-    public account: UserAccount;
+  public denominator: bigint;
+  public account: UserAccount;
 
-    public static async create(sk: Uint8Array, networkName: string, denominator: bigint): Promise<ZeroPoolState> {
-        const zpState = new ZeroPoolState();
-        zpState.denominator = denominator;
-        const userId = bufToHex(hash(sk));
-        const state = await UserState.init(`zp.${networkName}.${userId}`);
+  public static async create(sk: Uint8Array, networkName: string, denominator: bigint): Promise<ZeroPoolState> {
+    const zpState = new ZeroPoolState();
+    zpState.denominator = denominator;
+    const userId = bufToHex(hash(sk));
+    const state = await UserState.init(`zp.${networkName}.${userId}`);
 
-        try {
-            const acc = new UserAccount(sk, state);
-            zpState.account = acc;
-        } catch (e) {
-            console.error(e);
-        }
-
-        return zpState;
+    try {
+      const acc = new UserAccount(sk, state);
+      zpState.account = acc;
+    } catch (e) {
+      console.error(e);
     }
 
-    public getTotalBalance(): string {
-        return this.getBalances()[0];
-    }
+    return zpState;
+  }
 
-    public getBalances(): [string, string, string] {
-        const total = BigInt(this.account.totalBalance()) * this.denominator;
-        const acc = BigInt(this.account.accountBalance()) * this.denominator;
-        const note = BigInt(this.account.noteBalance()) * this.denominator;
+  public getTotalBalance(): string {
+    return this.getBalances()[0];
+  }
 
-        return [total.toString(), acc.toString(), note.toString()];
-    }
+  public getBalances(): [string, string, string] {
+    const total = BigInt(this.account.totalBalance()) * this.denominator;
+    const acc = BigInt(this.account.accountBalance()) * this.denominator;
+    const note = BigInt(this.account.noteBalance()) * this.denominator;
 
-    public free(): void {
-        this.account.free();
-    }
+    return [total.toString(), acc.toString(), note.toString()];
+  }
+
+  public free(): void {
+    this.account.free();
+  }
 }
